81eabce7ffeeed5ba2ea78705cbcf49d0f229f79
Paula <paulaperussi@gmail.com>
2019-10-11 19:34:08 -0300

    cadastro com devise

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class Player < ApplicationRecord
+  # frozen_string_literal: true.
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
